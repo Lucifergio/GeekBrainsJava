7f92e6f1c464c69059a9830d17f56eaeb469e4be
Aleksey <lexasalnikov@gmail.com>
2021-08-01 15:54:13 +0500

    HomeWorkApp3

--- src/main/java/Lesson3/HomeWorkApp3.java
+++ src/main/java/Lesson3/HomeWorkApp3.java
@@ -24,6 +24,7 @@ public class HomeWorkApp3 {
 
     }
 
+    // Задание номер 1
     public static void arraySwitchNum() {
         int[] arr = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 
@@ -38,6 +39,7 @@ public class HomeWorkApp3 {
         System.out.println(Arrays.toString(arr));
     }
 
+    // Задание номер 2
     public static void arrayFill() {
         int[] arr = new int[100];
 
@@ -48,6 +50,7 @@ public class HomeWorkApp3 {
         System.out.println(Arrays.toString(arr));
     }
 
+    // Задание номер 3
     public static void arrayCycle() {
         int[] arr = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
 
@@ -60,6 +63,7 @@ public class HomeWorkApp3 {
         System.out.println(Arrays.toString(arr));
     }
 
+    // Задание номер 4
     public static void arrDrawDiag() {
         int[][] arr = new int[5][5];
         int counter = 1;
@@ -89,7 +93,6 @@ public class HomeWorkApp3 {
                 } else {
                     System.out.print(arr[i][j] + " ");
                 }
-
                 ++counter;
             }
 
@@ -98,12 +101,14 @@ public class HomeWorkApp3 {
 
     }
 
+    // Задание номер 5
     public static int[] returnArrayLen(int len, int initialValue) {
         int[] arr = new int[len];
         Arrays.fill(arr, initialValue);
         return arr;
     }
 
+    // Задание номер 6*
     public static void arrayMinAndMaxNum() {
         int[] arr = new int[]{5, 2, 10, 13, 4, 7};
         Arrays.sort(arr);
@@ -111,6 +116,7 @@ public class HomeWorkApp3 {
         System.out.println("Максимальное число: " + arr[(arr.length - 1)]);
     }
 
+    // Задание номер 7**
     public static boolean sumLeftAndRightEqual(int[] arr) {
         int sumRight = 0;
         int sumLeft = 0;
@@ -127,17 +133,26 @@ public class HomeWorkApp3 {
         return false;
     }
 
+    // Задание номер 8***
     public static void shiftByNNumber (int[] arr, int num) {
-        int varNumFront = 0;
-        int varNumBack= 0;
-        for (int i = 0; i < arr.length-1; i++) {
+        int varNumFront;
+        int varNumMed;
+        int varNumBack = arr[arr.length - 1];
+        for (int i = 0; i < arr.length-2; i++) {
             varNumFront = arr[i];
-            varNumBack = arr[i+num];
-            if (varNumBack == 1) {
-                varNumBack = 5;
+            varNumMed = arr[i+num];
+            if (i == 0) {
+                arr[i+num] = varNumFront;
+                arr[arr.length -1] = varNumMed;
+            } else if (i == 1) {
+                varNumFront = arr[i];
+                varNumMed = arr[i+num];
+                arr[i-1] = varNumMed;
+                arr[i+num] = varNumFront;
+                arr[i] = varNumBack;
             }
-            arr[i+1+num] = varNumFront;
-            arr[i] = varNumBack;
+
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
